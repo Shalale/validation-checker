d9a784c7451625821a7c0e6c2e4fdb51ceba0a90
Shalale <selale.memmedli11@gmail.com>
2023-11-07 16:44:43 +0400

    added more test cases

--- src/test/java/PhoneNumberValidatorTest.java
+++ src/test/java/PhoneNumberValidatorTest.java
@@ -15,11 +15,17 @@ public class PhoneNumberValidatorTest {
     public void testValidPhoneNumber() {
         String phoneNumber = "0 0 30 69 74 0 9 22 52";
         assertTrue(validation.isValid(phoneNumber));
+
+        phoneNumber = "2 10 69 30 6 6 4";
+        assertTrue(validation.isValid(phoneNumber));
     }
 
     @Test
     public void testInvalidPhoneNumber() {
-        String phoneNumber = "123 abc"; // Contains non-numeric characters
+        String phoneNumber = "30 2 5 58";
+        assertFalse(validation.isValid(phoneNumber));
+
+        phoneNumber = "2 10 69 30 6 60 4";
         assertFalse(validation.isValid(phoneNumber));
     }
 
@@ -44,8 +50,8 @@ public class PhoneNumberValidatorTest {
     @Test
     public void testSimpleValidationOutputInvalid() {
         Output output = new Output();
-        String phoneNumber = "123 abc"; // Contains non-numeric characters
-        String expectedOutput = "123abc     [phone number: INVALID]";
+        String phoneNumber = "2 10 69 30 6 60 4";
+        String expectedOutput = "21069306604     [phone number: INVALID]";
 
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
